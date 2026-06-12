f98e6c5a7869164323ba7122650623990f06a815
sidhhay <abrolsidhhay596@gmail.com>
2026-06-12 16:56:55 +0530

    created admin signup endpoint

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,56 @@
 const {Router}=require("express");
 const adminRouter=Router();
 const {adminModel}=require("../db");
-adminRouter.post("/signup",function(req,res){
-    
+const z=require("zod");
+const bcrypt=require('bcrypt');
+const saltRounds=10;
+adminRouter.post("/signup",async function(req,res){
+    const schema=z.object({
+    email:z.string().email(),
+    firstname:z.string(),
+    lastname: z.string()
+    })
+    const email=req.body.email;
+    const firstname=req.body.firstname;
+    const lastname=req.body.lastname
+    const result=schema.safeParse({
+        email:email,
+        firstname:firstname,
+        lastname:lastname
+    })
+    if(!result.success){
+        return res.status(400).json({
+            message:"Invalid input",
+            errors:result.error.issues
+        })
+    }
+    const data=result.data;
+    const password=req.body.password;
+    if(!password || password.length<6){
+        return res.status(400).json({
+            message:"password must be atleast 6 charecters."
+        })
+    }
+
+    const hashedPassword=await bcrypt.hash(password,saltRounds);
+    const existingAdmin=await adminModel.findone({
+        email:data.email
+    })
+    if(existingAdmin){
+        return res.status(409).json({
+            message:"Admin already exists"
+        })
+    }
+    await adminModel.create({
+        email:data.email,
+        password:hashedPassword,
+        firstname:data.firstname,
+        lastname:data.lastname
+
+    });
+    res.json({
+        message:"signup succesfull"
+    })
 });
 adminRouter.post("/signin",function(req,res){
     
